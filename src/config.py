1f62946a4fdcb353c63b3739dafb8bf995840156
harshit sharma <harshit158@gmail.com>
2021-03-01 22:40:01 -0500

    now using 2020 models instead of 2018 models

--- src/config.py
+++ src/config.py
@@ -1,12 +1,12 @@
 # Input paths for pretrained models
 CONSTITUENCY_PARSING = {
-    'web_path': "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo-constituency-parser-2018.03.14.tar.gz",
-    'local_path': '../pretrained_weights/elmo-constituency-parser-2018.03.14.tar.gz'
+    'web_path': "https://storage.googleapis.com/allennlp-public-models/elmo-constituency-parser-2020.02.10.tar.gz",
+    'local_path': '../pretrained_weights/elmo-constituency-parser-2020.02.10.tar.gz'
 }
 
 OPENIE = {
-    'web_path': "https://s3-us-west-2.amazonaws.com/allennlp/models/openie-model.2018-08-20.tar.gz",
-    'local_path': '../pretrained_weights/openie-model.2018-08-20.tar.gz'
+    'web_path': "https://storage.googleapis.com/allennlp-public-models/openie-model.2020.03.26.tar.gz",
+    'local_path': '../pretrained_weights/openie-model.2020.03.26.tar.gz'
 }
 
 # location to store annotated PDF
